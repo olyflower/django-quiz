a332883a22d5d9411334c4f2492ec30197a24d6d
olyflower <2013.olga.yudina@gmail.com>
2023-06-26 14:34:48 +0300

    Added postgres

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -8,12 +8,32 @@ SECRET_KEY = "y=x&59)5q)mg0y05xqi9@*-)nti8e8@b07e2wk@0l$a9+zhh-n"
 
 ALLOWED_HOSTS = []
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.environ.get("GITHUB_WORKFLOW"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": "postgres",
+            "USER": "postgres",
+            "PASSWORD": "postgres",
+            "HOST": "0.0.0.0",
+            "PORT": 5432,
+        }
     }
-}
+else:
+    DATABASES = {
+        "default_sql": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
+        },
+    }
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
