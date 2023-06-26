06f9ca15adce0dc939a51433308d34b18edf2f7e
olyflower <2013.olga.yudina@gmail.com>
2023-06-26 23:59:15 +0300

    Added nginx, start_server_prod.sh

--- src/config/settings/prod.py
+++ src/config/settings/prod.py
@@ -6,13 +6,28 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
+
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = BASE_DIR / "static_content/"
+STATIC_URL = "/static/"
+
+MEDIA_ROOT = BASE_DIR / "media_content/"
+MEDIA_URL = "/media/"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "default_sql": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        },
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.environ.get("POSTGRES_DB"),
+            "USER": os.environ.get("POSTGRES_USER"),
+            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+            "HOST": os.environ.get("POSTGRES_HOST"),
+            "PORT": os.environ.get("POSTGRES_PORT"),
+        },
     }
-}
-
-STATIC_URL = "static/"
